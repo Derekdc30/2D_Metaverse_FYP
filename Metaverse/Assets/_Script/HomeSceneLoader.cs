f20f3a12b297fed17cef28924fef7fd073c489bc
Metaverse <derekchoy30@gmail.com>
2024-01-29 00:00:44 +0800

    1/29

--- Metaverse/Assets/_Script/HomeSceneLoader.cs
+++ Metaverse/Assets/_Script/HomeSceneLoader.cs
@@ -11,49 +11,70 @@ public class HomeSceneLoader : MonoBehaviour
 {
     private int _stackedSceneHandle;
     public bool SceneStack = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         NetworkObject nob = other.GetComponent<NetworkObject>();
-        if(nob != null){
-            LoadScene(nob);
+        if (nob != null)
+        {
+            LoadScene(nob, true);
         }
     }
-    void LoadScene(NetworkObject nob)
-    { 
-        if(!nob.Owner.IsActive){return;} 
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        NetworkObject nob = other.GetComponent<NetworkObject>();
+        if (nob != null)
+        {
+            LoadScene(nob, false);
+        }
+    }
+
+    void LoadScene(NetworkObject nob, bool entering)
+    {
+        if (!nob.Owner.IsActive) { return; }
 
         SceneLookupData lookup;
-        if(this.tag == "TP_Home" ){
-            lookup = new SceneLookupData(_stackedSceneHandle,"HomeScene");
+        if (this.tag == "TP_Home")
+        {
+            lookup = new SceneLookupData(_stackedSceneHandle, "HomeScene");
         }
-        else if(this.tag == "TP_Main"){
-            // Unload the home scene when the player leaves it.
-            InstanceFinder.SceneManager.UnloadConnectionScenes(nob.Owner, new SceneUnloadData(new SceneLookupData(_stackedSceneHandle,"HomeScene")));
-            lookup = new SceneLookupData(_stackedSceneHandle,"MainScene");
+        else if (this.tag == "TP_Main")
+        {
+            if (entering)
+            {
+                // Unload the home scene when the player enters it.
+                InstanceFinder.SceneManager.UnloadConnectionScenes(nob.Owner, new SceneUnloadData(new SceneLookupData(_stackedSceneHandle, "HomeScene")));
+            }
+            lookup = new SceneLookupData(_stackedSceneHandle, "MainScene");
         }
-        else{
-            lookup = new SceneLookupData(_stackedSceneHandle,"MainScene");
+        else
+        {
+            lookup = new SceneLookupData(_stackedSceneHandle, "MainScene");
         }
 
-        SceneLoadData sld =  new SceneLoadData(lookup);
+        SceneLoadData sld = new SceneLoadData(lookup);
         sld.Options.AllowStacking = true;
-        sld.MovedNetworkObjects = new NetworkObject[] {nob};
+        sld.MovedNetworkObjects = new NetworkObject[] { nob };
         sld.ReplaceScenes = ReplaceOption.All;
-        InstanceFinder.SceneManager.LoadConnectionScenes(nob.Owner,sld);
+        InstanceFinder.SceneManager.LoadConnectionScenes(nob.Owner, sld);
     }
 
-
-
-    private void Start() {
+    private void Start()
+    {
         InstanceFinder.SceneManager.OnLoadEnd += SceneManager_OnLoadEnd;
     }
-    private void OnDestroy() {
-        if(InstanceFinder.SceneManager != null) InstanceFinder.SceneManager.OnLoadEnd -= SceneManager_OnLoadEnd;
+
+    private void OnDestroy()
+    {
+        if (InstanceFinder.SceneManager != null) InstanceFinder.SceneManager.OnLoadEnd -= SceneManager_OnLoadEnd;
     }
-    private void SceneManager_OnLoadEnd(SceneLoadEndEventArgs obj){
-        if(!obj.QueueData.AsServer) return;
-        if(!SceneStack) return;
-        if(_stackedSceneHandle != 0)return;
-        if(obj.LoadedScenes.Length>0) _stackedSceneHandle = obj.LoadedScenes[0].handle;
+
+    private void SceneManager_OnLoadEnd(SceneLoadEndEventArgs obj)
+    {
+        if (!obj.QueueData.AsServer) return;
+        if (!SceneStack) return;
+        if (_stackedSceneHandle != 0) return;
+        if (obj.LoadedScenes.Length > 0) _stackedSceneHandle = obj.LoadedScenes[0].handle;
     }
 }
