bb6591445914bdb30604173c76457c7b9d0c85fa
sstareven <45038473+sstareven@users.noreply.github.com>
2024-03-21 15:31:19 +0800

    3/21 Animator
    Players now can normally play the animation when moving and idle

--- Metaverse/Assets/_Script/Player/PlayerMovement.cs
+++ Metaverse/Assets/_Script/Player/PlayerMovement.cs
@@ -10,24 +10,57 @@ public class PlayerMovement : NetworkBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed = 1f;
+    //public GameObject playerCamera;
     private Rigidbody2D rb;
-    private PlayerAnimating _animating;
+    private Animator animator;
+    private Vector2 movement;
+    private string currentAnimation = "";
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if(base.IsOwner)
+        {
+            //playerCamera.SetActive(true);
+            animator = GetComponent<Animator>();
+        }
+    }
+
     private void Start(){
         rb = GetComponent<Rigidbody2D>();
         GameObject player = GameObject.FindGameObjectWithTag("Player");     
         Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
-    private void Awake() {
-        _animating = GetComponent<PlayerAnimating>();
-    }
+
+   
+  
     private void Update(){
         if(!base.IsOwner){return;}
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+        movement = new Vector2(moveHorizontal,moveVertical);
         rb.velocity = movement* moveSpeed;
 
-        bool moving = (moveHorizontal != 0f || moveVertical != 0f);
-        _animating.SetMoving(moving);
+        //bool moving = (moveHorizontal != 0f || moveVertical != 0f);
+        CheckAnimation();
+    }
+
+    private void CheckAnimation()
+    {
+        if(rb.velocity.x != 0 || rb.velocity.y != 0)
+        {
+            ChangeAnimation("Moving");
+        }
+        else
+            ChangeAnimation("Idle");
+    }
+     private void ChangeAnimation(string animation, float crossfade = 0.2f)
+    {
+        if(currentAnimation != animation)
+        {
+            currentAnimation = animation;
+            animator.CrossFade(animation, crossfade);
+        }
     }
 }
